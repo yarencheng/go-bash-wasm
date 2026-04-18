4b4889d3274c5ea510b7779738cbab07ac232da2
Aren Chen <yarencheng@gmail.com>
2026-04-18 14:56:12 +0800

    feat: add crypto donation addresses to version output

--- internal/app/app.go
+++ internal/app/app.go
@@ -152,6 +152,8 @@ var (
 	BashLicense   = "License Apache-2.0: Apache License, Version 2.0 <http://www.apache.org/licenses/LICENSE-2.0>\n"
 	ProjectURL    = "https://bash.devops-playground.dev/"
 	SourceURL     = "https://github.com/yarencheng/go-bash-wasm"
+	BtcAddress    = "bc1qntk3pkvlkd9kg8kgjyff85rplyal7jl7t3pyxl"
+	EthAddress    = "0xfC689a03D0BF58b27f1928eed952CAbCF816fAE9"
 )
 
 // App encapsulates the bash simulator application.
@@ -402,6 +404,8 @@ func (a *App) ShowVersion() {
 	fmt.Fprintf(a.Env.Stdout, "%s\n", BashLicense)
 	fmt.Fprintf(a.Env.Stdout, "Home page:   <%s>\n", ProjectURL)
 	fmt.Fprintf(a.Env.Stdout, "Source code: <%s>\n", SourceURL)
+	fmt.Fprintf(a.Env.Stdout, "Donation (BTC): <%s>\n", BtcAddress)
+	fmt.Fprintf(a.Env.Stdout, "Donation (ETH): <%s>\n", EthAddress)
 }
 
 func setupMockFiles(fs afero.Fs, logger zerolog.Logger) {
